44068e0e347fb6b145124eb01d18f2b34c45922f
BhaveshMakwana27 <makvanab492@gmail.com>
2024-08-26 22:14:57 +0530

    1.0

--- temp.py
+++ temp.py
@@ -1,25 +1,9 @@
-from fastapi import FastAPI,Form
-from fastapi.responses import FileResponse
-from enum import Enum
-from pydantic import BaseModel
+from fastapi import FastAPI
 
-# class Status(Enum):
-#     TO_DO = "to_do"
-#     IN_PROGRESS = "in_progress"
-#     DONE = "done"
-
-# class Task(BaseModel):
-#     title: str
-#     status: Status
 
 app = FastAPI()
 
 @app.get('/')
 def index():
 
-    return FileResponse("image-512x512.jpg")
-
-@app.get('/img')
-def index():
-
-    return FileResponse("image-512x512.jpg")
+    return True
